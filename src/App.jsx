0b3e0c2155c897f1ed89ccadc4537aa120fe8eb0
uhohjackieson <jaclynbarbieri@gmail.com>
2023-08-03 17:12:41 -0400

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import { puppyList } from './data.js'
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
+import "./style.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  const featuredPup = puppies.find((pup)=> pup.id ===featPupId);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup);
-  
+
   console.log(puppies);
   return (
     <>
       {featPupId && (
-        <div>
+        <div className= "container">
           <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
@@ -20,15 +21,20 @@ function App() {
           </ul>
         </div>
       )}
-        {
-          puppies.map((puppy) => {
-            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
-          })
-        }
-
-
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
