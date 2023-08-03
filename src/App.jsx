e6e1856fd998486c2380f53a56729475a224a71d
uhohjackieson <jaclynbarbieri@gmail.com>
2023-08-03 17:00:42 -0400

    before css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id ===featPupId);
+  console.log(featuredPup);
+  
+  console.log(puppies);
   return (
     <>
-      <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        {
+          puppies.map((puppy) => {
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
 
-      </div>
 
     </>
   )
